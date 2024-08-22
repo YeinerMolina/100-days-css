c1bc77ffbba8030f806ccd7247e6487dca87dd73
Yeiner Molina <ingyeinermolina@gmail.com>
2024-08-21 22:56:03 -0500

    Day three

---
+++ src/app/components/challenge/day-three/day-three.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-three',
+  standalone: true,
+  templateUrl: './day-three.component.html',
+  styleUrl: './day-three.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DayThreeComponent {}
--- src/app/components/challenge/index.ts
+++ src/app/components/challenge/index.ts
@@ -1,4 +1,5 @@
 import { DayOneComponent } from './day-one/day-one.component';
+import { DayThreeComponent } from './day-three/day-three.component';
 import { DayTwoComponent } from './day-two/day-two.component';
 
-export const days = [DayOneComponent, DayTwoComponent];
+export const days = [DayOneComponent, DayTwoComponent, DayThreeComponent];
