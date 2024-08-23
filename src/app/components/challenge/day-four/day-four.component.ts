da350d5c8dfe49a0a7268ac6216ddf9c2924b82f
Yeiner Molina <ingyeinermolina@gmail.com>
2024-08-22 22:19:34 -0500

    Day four

---
+++ src/app/components/challenge/day-four/day-four.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-four',
+  standalone: true,
+  imports: [],
+  templateUrl: './day-four.component.html',
+  styleUrl: './day-four.component.css'
+})
+export class DayFourComponent {
+
+}
--- src/app/components/challenge/index.ts
+++ src/app/components/challenge/index.ts
@@ -1,5 +1,11 @@
+import { DayFourComponent } from './day-four/day-four.component';
 import { DayOneComponent } from './day-one/day-one.component';
 import { DayThreeComponent } from './day-three/day-three.component';
 import { DayTwoComponent } from './day-two/day-two.component';
 
-export const days = [DayOneComponent, DayTwoComponent, DayThreeComponent];
+export const days = [
+  DayOneComponent,
+  DayTwoComponent,
+  DayThreeComponent,
+  DayFourComponent,
+];
